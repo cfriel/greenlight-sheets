9e48a5486abfb3bdc8b617fede1eafdbe07c8890
Chris Friel <cfriel@gmail.com>
2013-08-05 15:29:02 -0400

    moving packages

--- server/sheets.js
+++ server/sheets.js
@@ -1,10 +1,16 @@
 var name = "sheets";
 var version = "1.0";
 
+sheets = function(){};
+
+sheets.prototype = new sheets();
+
+Sheets = sheets.prototype;
+
 Meteor.startup(function(){
     
     console.log("loading sheets package");
     
-    Greenlight.register_template(name, version);
+    Greenlight.register_template(name, version, Sheets);
 
 });
