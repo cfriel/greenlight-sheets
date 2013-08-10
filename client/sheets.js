f462c79cd5551d035f7dd72d0f16bcb7bf883317
Chris Friel <cfriel@gmail.com>
2013-08-10 03:00:21 -0400

    updating sheets / dataset page

--- client/sheets.js
+++ client/sheets.js
@@ -1,31 +1,9 @@
 Meteor.Router.add({
-    '/sheets*': function(path)
+   
+    '/sheets/:dataset': function(dataset)
     {
-	if(path.length == 0)
-	{
-	}
-	else
-	{
-	    if(path.substring(0,1) == "/")
-	    {
-		path = path.substring(1, path.length);
-	    }
+	Session.set("sheets_dataset", dataset);
 
-	    var splits = path.split("/");
-
-	    if(splits.length == 2)
-	    {
-		var database = splits[0];
-		var collection = splits[1];
-
-		Session.set("sheets_page_database", database);
-		Session.set("sheets_page_collection", collection);
-		
-		return 'sheets_page';
-	    }
-
-	}
-	
 	return 'sheets_page';
     }
 
--- client/sheets_page.js
+++ client/sheets_page.js
@@ -1,77 +1,36 @@
-Template.sheets_page.rendered = function() 
-{
+Deps.autorun(function(){
+    
     var data = [];
     var columns = [];
     
-    var options = {
-	enableCellNavigation: true,
-	enableColumnReorder: false,
-	rowHeight: 30,
-	editable: false,
-	autoEdit: true,
-	forceFitColumns: true
-    };
-    
-    var grid = new Slick.Grid("#sheets-container", data, columns, options);
+    var datasetName = Session.get("sheets_dataset");
 
-    grid.setData({});
-    
-    var database = Session.get("sheets_page_database");
-    var collection = Session.get("sheets_page_collection");
-    
-    if(collection != undefined && database != undefined)
+    var dataset = Greenlight.Dataset.Datasets.findOne({name: datasetName});
+
+    if(dataset != undefined)
     {
-	Meteor.call('load', database, collection, {}, 0, 100, function(loadErr, loadResult)
-		    {
-			Meteor.call('schema', database, collection, function(schemaErr, schemaResult)
-				    {
-					var schema = schemaResult;
-					
-					var query = { _collection : collection };
-					
-					var data = Data.find(query, {limit : 100}).fetch();
-					
-					var columns = [];
-					var keys = Object.keys(schema);
-					
-					for(var i = 0; i < keys.length; i++)
-					{
-					    columns[i] = { id: keys[i], name: keys[i], field: keys[i], minWidth: 120, editor: Slick.Editors.Text, sortable: true };
-					}
-					
-					grid = new Slick.Grid("#sheets-container", data, columns, options);
-
-					grid.onSort.subscribe(function(e, args){ // args: sort information. 
-					    var field = args.sortCol.field;
-					    
-					    data.sort(function(a, b){
-						var result = 
-						    a[field] > b[field] ? 1 :
-						    a[field] < b[field] ? -1 :
-						    0;
-						
-						return args.sortAsc ? result : -result;
-					    });
-					    
-					    grid.invalidate();         
-					});
-					
-					grid.setSelectionModel(new Slick.CellSelectionModel()); 
-					
-				    });
-		    });
+	var schema = dataset.schema;
+	
+	var query = { _collection : dataset.collection };
+	
+	var data = Data.find(query, {limit : 100}).fetch();
+	
+	var columns = [];
+	var keys = Object.keys(schema);
+	
+	for(var i = 0; i < keys.length; i++)
+	{
+	    columns[i] = { id: keys[i], name: keys[i], field: keys[i], minWidth: 120, editor: Slick.Editors.Text, sortable: true };
+	}
+	
+	var grid = Greenlight.Helpers.create_slickgrid("#sheets-container", data, columns);
+	
     }
 
-    $('#sheets-container').height($(window).height() - 70);
 
-    $(window).resize(function() {
-	$('#sheets-container').height($(window).height() - 70);
-	$(".slick-viewport").height($("#sheets-container").height() - 35);
-    });
+});
+
+Template.sheets_page.rendered = function() 
+{
     
-    
-}
-
-
-
-
+};
